fee395b092ed8961511685b5340753325f159d4c
yuriileaoo <77992176+yuriileaoo@users.noreply.github.com>
2023-09-22 10:47:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,16 @@ http.createServer(async function (req, res) {
         .then(async (response) => {
             console.log(response)
             console.log(response.data)
+            res.send(response.data)
             return response.data
         })
         .catch((error) => {
+            res.send(error)
             return error
         })
     
 
     
-    res.write('Yo!');
-    res.end();
+    // res.write('Yo!');
+    // res.end();
 }).listen(process.env.PORT || 3000);
