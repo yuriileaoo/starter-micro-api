0ee673f9968e0453cde9c32b0136773e764e250b
yuriileaoo <77992176+yuriileaoo@users.noreply.github.com>
2023-09-22 09:44:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
+    
+    let url = "https://copel.nexenergy.com.br/api/massive-sender";
+
+      try {
+          // fetch is available with Node.js 18
+          const res = await fetch(url);
+          if (res.ok) {
+            const data = await res.json();
+            console.log(data);
+          }
+          console.info("status", res.status, res, res.json);
+          return res.json;
+      }
+      catch (e) {
+          console.error(e);
+          return 500;
+      }
+
+    
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
